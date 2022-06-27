83e744c5d8c374bcafe0e2bb0ac6ff150c788132
LluisSalas <lluissalaslozano@gmail.com>
2022-06-27 19:16:40 +0200

    initial commit

---
+++ apps/nextail-products-report/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nextail-tech-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nextail-products-report';
+}
